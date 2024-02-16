4e969c422b26bde63d9c03f663d4d2277fc343f9
samynaj <samuelnnj@gmail.com>
2024-02-16 11:40:39 +0100

    obelle app commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import OnboardingScreen from "./screens/OnboardingScreen";
+import Slider from "./screens/Slider";
+import Form from "./screens/Form";
+import FAQScreen from "./screens/FAQScreen";
+import { useFonts } from "expo-font";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
+  const [loaded] = useFonts({
+    Pacifico: require("./assets/fonts/Pacifico-Regular.ttf"),
+    Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View>
+    //   <Text>hello height</Text>
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name={"Onboarding"} component={OnboardingScreen} />
+        <Stack.Screen name={"Slider"} component={Slider} />
+        <Stack.Screen name={"Form"} component={Form} />
+        <Stack.Screen name={"FAQ"} component={FAQScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
